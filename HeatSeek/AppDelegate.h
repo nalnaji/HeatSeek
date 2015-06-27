d7303287c57cc181bd9c27957fb163afc1c6293c
Naseem Alnaji <nalnaji@berkeley.edu>
2015-06-27 00:28:38 -0700

    initial commit

---
+++ HeatSeek/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeatSeek
+//
+//  Created by Naseem Al-Naji on 6/27/15.
+//  Copyright (c) 2015 Naseem Al-Naji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HeatSeek/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HeatSeek
+//
+//  Created by Naseem Al-Naji on 6/27/15.
+//  Copyright (c) 2015 Naseem Al-Naji. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
