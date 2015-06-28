90b5635c7a192607d19c2a11323cd3cacd67cddf
Naseem Alnaji <nalnaji@berkeley.edu>
2015-06-27 20:09:03 -0700

    home view feature

---
+++ HeatSeek/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  HeatSeek
+//
+//  Created by Naseem Al-Naji on 6/27/15.
+//  Copyright (c) 2015 Naseem Al-Naji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
